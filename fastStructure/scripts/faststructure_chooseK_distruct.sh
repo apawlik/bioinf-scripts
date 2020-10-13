60fdd4a6731c9ea6259356198bf07e6a51f73fbe
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2020-10-14 12:24:51 +1300

    fix path typo and for loop

--- fastStructure/scripts/faststructure_chooseK_distruct.sh
+++ fastStructure/scripts/faststructure_chooseK_distruct.sh
@@ -1,7 +1,7 @@
+#!/bin/bash -e
+
 # This script was developed by Manpreet Dhami (Manaaki Whenua Landcare Research) and Ben Roberts (NeSI NZ)
 
-
-#!/bin/bash -e
 
 ###########################
 # Setup SLURM Environment #
@@ -10,7 +10,7 @@
 #SBATCH --account YOUR_PROJECT_NAME
 #SBATCH --job-name chooseK
 #SBATCH --cpus-per-task 4
-#SBATCH --time 3:00:00
+#SBATCH --time 30:00
 #SBATCH --mem-per-cpu=1G
 #SBATCH --output chooseK-%j.out
 #SBATCH --error chooseK-%j.err
@@ -21,20 +21,16 @@ workingdir=$HOME/fastStructure_workshop
 vcfdir="${workingdir}/data"
 #Genome="MR1_a10"
 scripts="${workingdir}/scripts"
-prior="simple"
+prior="simple" #This needs to either simple or logistic
 
 #Modules
 module load Python/3.6.3-gimkl-2017a
 module load fastStructure/1.0-gimkl-2017a-Python-2.7.14
 
 
-# Need to add module loading 
-# See https://support.nesi.org.nz/hc/en-gb/articles/360000474535-Installing-Third-Party-applications
-
-
 for K in 1 2 3 4 5 6 7 8 9 10 
 		do
-python "${EBROOTFASTSTRUCTURE}/bin/distruct.py" \
+python distruct.py \
 			-K $K \
 			--input=${vcfdir}/output/${prior}/final_strains \
 			--output=${vcfdir}/output/${prior}/final_strains_"$K".svg
