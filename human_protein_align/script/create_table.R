c28d1b232f13e01649d0003dc82ecf4bfcbc74c4
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2021-02-23 14:17:12 +1300

    Fixed the issue with the path to data

--- human_protein_align/script/create_table.R
+++ human_protein_align/script/create_table.R
@@ -3,12 +3,12 @@ library("tidyverse")
 #loop through the files with comparison between human genome
 # and create a data frame which then can be saved as CSV
 
-files = list.files( path="data", pattern="*_stats", full.names=TRUE)
+files = list.files( path="../data", pattern="*_stats", full.names=TRUE)
 human_animal_df <- data.frame(human_prot=character(), 
                               stringsAsFactors = FALSE)
 
 for(i in 1:length(files)){
-  stats <- read.table(files[i])
+  stats <- read.table( files[i] )
   if (i == 1) {
   human_animal_df[nrow(human_animal_df)+1, ] <- c(stats$V1[1])
   human_animal_df[ ,ncol(human_animal_df)+1] <- c(stats$V1[3])
@@ -21,4 +21,4 @@ for(i in 1:length(files)){
 
 }
 
-
+write.table(human_animal_df, "output.csv", sep=",", row.names=FALSE, col.names=TRUE) 
