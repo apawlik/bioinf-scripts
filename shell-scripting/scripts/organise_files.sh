4b1be070a0c05bac8e72a4a89d7374bb30cc1479
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2021-04-22 14:50:00 +1200

    notes

--- shell-scripting/scripts/organise_files.sh
+++ shell-scripting/scripts/organise_files.sh
@@ -2,11 +2,16 @@
 # This is an example script for organising your workspace (files and directories)
 # before you start anaylysing data. 
 
+## Good idea is to define all variables at the beginning of your scipt
+FASTQ_SOURCE=https://github.com/apawlik/bioinf-scripts/raw/master/shell-scripting/data/untrimmed_fastq.zip  #Change to wherever you have data source
+
+
 ## STEP 1
 ## 1.1 Make sure we are in home directory.
-## 1.2 create a project directory (if it doesn't exist) and navigate into it
 
 cd $HOME
+
+## 1.2 create a project directory (if it doesn't exist) and navigate into it
 
 if [ -d my_project ] 
 then
@@ -19,7 +24,6 @@ cd my_project
 
 ## STEP 2 Get the data
 ## 2.1 Create a directory for the data 
-## 2.2 Download the data from online source
 if [ -d data ]
 then
    echo "You already set up a directory for your data." 
@@ -29,17 +33,17 @@ fi
 
 cd data
 
-curl -L https://github.com/apawlik/bioinf-scripts/raw/master/shell-scripting/data/untrimmed_fastq.zip -o untrimmed_fastq.zip
-
+## 2.2 Download the data from online source
+curl -L $FASTQ_SOURCE -o untrimmed_fastq.zip
 unzip untrimmed_fastq.zip
 mv untrimmed_fastq.zip/* .
 rm -r untrimmed_fastq.zip/
-
 
 curl -L https://github.com/apawlik/bioinf-scripts/raw/master/shell-scripting/data/sample-data.zip -o sample-data.zip
 unzip sample-data.zip
 mv sample-data.zip/* .
 rm -r sample-data.zip/
+
 
 ## STEP 3 sort the data files into 
 ### 3.1 create sub-folders for data
@@ -53,23 +57,27 @@ fi
 
 for filename in *
 do 
+   if [ -f $filename]
+   then
    case $filename in 
       *.fasta)
          echo "Moving $filename into fasta directory"
+         mv $finlaname fasta/
          ;;
       *)
          ;;
-     esac
+   esac
+   fi
 done
 
 ########### End of section for organising data ##################
 
 
 
-## Pre-cleaning data
-##########
+## Pre-cleaning data##########
 
 cd $HOME/my_project/data/untrimmed_fastq
+
 for filename in *
 do 
   if [ "grep -n "NNN" $filename" ]
